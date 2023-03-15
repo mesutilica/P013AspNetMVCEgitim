561a9b9a27e610dd2a5a325123d80844271d1a84
mesutilica <mesut.ilica@gmail.com>
2023-03-15 22:07:36 +0300

    15-03-2023 model binding deyiz

---
+++ P013AspNetMVCEgitim/Controllers/MVC04ModelBindingController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using P013AspNetMVCEgitim.Models;
+
+namespace P013AspNetMVCEgitim.Controllers
+{
+    public class MVC04ModelBindingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult KullaniciDetay()
+        {
+            Kullanici kullanici = new();
+            kullanici.KullaniciAdi = "Admin";
+            kullanici.Ad = "Murat";
+            kullanici.Soyad = "Yılmaz";
+            kullanici.Sifre = "123";
+            kullanici.Email = "Murat@yilmaz.co";
+            return View(kullanici); // View içerisinde model datası olarak kullanici nesnesini sayfaya gönderdik.
+        }
+        [HttpPost]
+        public IActionResult KullaniciDetay(Kullanici kullanici) // post metodunda modelden gelen nesneyi bu şekilde parantez içinde yakalayabiliyoruz
+        {
+            return View(kullanici); // gelen kullanici nesnesini tekrardan ekrana yolla
+        }
+        public IActionResult AdresDetay()
+        {
+            Adres adres = new()
+            {
+                Sehir = "Çankırı", Ilce = "Çerkeş", AcikAdres = "Çiçek sk. No : 18 A"
+            };
+            return View(adres);
+        }
+        public IActionResult UyeSayfasi()
+        {
+            Kullanici kullanici = new();
+            kullanici.KullaniciAdi = "Admin";
+            kullanici.Ad = "Murat";
+            kullanici.Soyad = "Yılmaz";
+            kullanici.Sifre = "123";
+            kullanici.Email = "Murat@yilmaz.co";
+
+            Adres adres = new()
+            {
+                Sehir = "Çankırı",
+                Ilce = "Çerkeş",
+                AcikAdres = "Çiçek sk. No : 18 A"
+            };
+
+            var model = new UyeSayfasiViewModel()
+            {
+                 Kullanici = kullanici,
+                 Adres = adres
+            };
+            return View(model);
+        }
+    }
+}
