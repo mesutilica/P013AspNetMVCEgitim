46f19891fb99f4de280bf034186c453a2df3cd73
mesutilica <mesut.ilica@gmail.com>
2023-03-27 23:00:48 +0300

    27-03-2023 session

--- P013AspNetMVCEgitim/Controllers/MVC08ViewResultsController.cs
+++ P013AspNetMVCEgitim/Controllers/MVC08ViewResultsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using P013AspNetMVCEgitim.Models;
 
 namespace P013AspNetMVCEgitim.Controllers
 {
@@ -19,9 +20,45 @@ namespace P013AspNetMVCEgitim.Controllers
             //return RedirectToAction("Index"); // metot çalıştığında aynı controllerdaki bir actiona yönlendirmemizi sağlar
             return RedirectToAction("Index", "Home"); // metot çalıştığında farklı bir controller daki actiona bu şekilde yönlendirebiliriz
         }
-        public IActionResult RouteYonlendir()
+        public RedirectToRouteResult RouteYonlendir() // IActionResult da yapsaydık olurdu
         {
             return RedirectToRoute("Default", new { controller="Home", action="Index", id = 18 }); // metot çalıştığında route sistemiyle yönlendirme yapmamızı sağlar
+        }
+        public PartialViewResult KategorileriGetirPartial() // IActionResult da yapsaydık olurdu
+        {
+            return PartialView("_KategorilerPartial");
+        }
+        public IActionResult XmlContentResult()
+        {
+            var xml = @"
+                    <Kullanicilar>
+                     <Kullanici>
+                        <Adi>
+                            Murat 
+                        </Adi>
+                        <Soyadi>
+                            Yılmaz 
+                        </Soyadi>
+                     </Kullanici>
+                     <Kullanici>
+                        <Adi>
+                            Alp 
+                        </Adi>
+                        <Soyadi>
+                            Arslan 
+                        </Soyadi>
+                     </Kullanici>
+                    </Kullanicilar>
+                ";
+            return Content(xml, "application/xml"); // geriye xml içeriğini döndürdük
+        }
+        public IActionResult JsonDondur()
+        {
+            var kullanici = new Kullanici()
+            {
+                 Ad = "Alp", Soyad = "Çakmak", KullaniciAdi = "alpi"
+            };
+            return Json(kullanici);
         }
     }
 }
